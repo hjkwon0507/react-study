de789705ca9c9af47f429002ce2a86091eef6085
hyejin-kwon <hjkwon0507@findconnect.co.kr>
2021-12-17 10:29:46 +0900

    Lotto

--- ZeroCho/RSP/client.jsx
+++ ZeroCho/RSP/client.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import Lotto from './Lotto';
+import RSP from './RSP';
 
-const Hot = hot(Lotto);
+const Hot = hot(RSP);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
