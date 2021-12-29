105670b054fac4823dc2b4827f0e94145e460c27
hyejin-kwon <hjkwon0507@findconnect.co.kr>
2021-12-29 16:42:47 +0900

    모각코 7일차

--- Mogakko/youtube-clone/src/App.js
+++ Mogakko/youtube-clone/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import youtubeData from './data/youtubeData.json';
+import Layout from './components/shared/Layout';
+import { useState } from 'react';
 
 function App() {
-  console.log('youtubeData : ', youtubeData.data[0].id + ' -> 영상의 링크 주소');
-  console.log('youtubeData : ', youtubeData.data[0].title + ' -> 영상의 제목');
-  console.log('youtubeData : ', youtubeData.data[0].date + ' -> 영상의 날짜');
-  console.log('youtubeData : ', youtubeData.data[0].thumbnail + ' -> 영상의 썸네일');
-  console.log('youtubeData : ', youtubeData.data[0].description + ' -> 영상의 설명');
-  console.log('youtubeData : ', youtubeData.data[0].category + ' -> 영상의 카테고리');
-  console.log('youtubeData : ', youtubeData.data[0].viewCount + ' -> 영상의 조회수');
-  console.log('youtubeData : ', youtubeData.data[0].likeCount + ' -> 영상의 좋아요수');
-  console.log('youtubeData : ', youtubeData.data[0].channelUrl + ' -> 채널의 url');
-  console.log('youtubeData : ', youtubeData.data[0].channelId + ' -> 채널의 링크 주소');
-  console.log('youtubeData : ', youtubeData.data[0].channelThumbnail + ' -> 채널의 썸네일');
-  console.log('youtubeData : ', youtubeData.data[0].channelTitle + ' -> 채널의 제목');
-  return <div>리액트로 데이터 불러오기</div>;
+  const [value, setValue] = useState(false);
+
+  function onClick() {
+    setValue(!value);
+  }
+
+  return (
+    <Layout>
+      <div>콘텐츠!</div>
+      <button onClick={onClick}>True?</button>
+      {value === true && <div>True!</div>}
+    </Layout>
+  );
 }
 
 export default App;
