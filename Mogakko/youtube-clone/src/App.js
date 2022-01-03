bef3c775cfa4f3d3151e78a9c7225c08f0ab005f
hyejin-kwon <hjkwon0507@findconnect.co.kr>
2022-01-03 18:12:33 +0900

    모각코 10일차 과제

--- Mogakko/youtube-clone/src/App.js
+++ Mogakko/youtube-clone/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/shared/Layout';
-import { useState } from 'react';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Subscription from './pages/Subscription';
--- Mogakko/youtube-clone/src/components/shared/Layout.js
+++ Mogakko/youtube-clone/src/components/shared/Layout.js
@@ -1,13 +1,20 @@
 import styles from './Layout.module.css';
 import Header from './Header.js';
 import Menu from './Menu.js';
+import { useState } from 'react';
 
-function Layout({ children }) {
+function Layout({ children, activeMenu }) {
+  const [menu, setMenu] = useState(false);
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header menu={menu} setMenu={setMenu} />
       <div className={styles.layout}>
-        <Menu />
+        {
+          menu === true
+          ? <Menu />
+          : null
+        }
         <div className={styles.contents}>{children}</div>
       </div>
     </div>
