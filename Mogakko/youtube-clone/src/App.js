ff5e530020e89b9b49b7f3f69914248caaad68d4
hyejin-kwon <hjkwon0507@findconnect.co.kr>
2022-01-03 13:55:38 +0900

    모각코 9일차 공부

--- Mogakko/youtube-clone/src/App.js
+++ Mogakko/youtube-clone/src/App.js
@@ -1,18 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/shared/Layout';
 import { useState } from 'react';
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import Subscription from './pages/Subscription';
 
 function App() {
-  const [value, setValue] = useState(false);
-
-  function onClick() {
-    setValue(!value);
-  }
-
   return (
-    <Layout>
-      <div>콘텐츠!</div>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/subscription" element={<Subscription />} />
+    </Routes>
   );
 }
 
