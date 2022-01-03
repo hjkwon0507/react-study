5f9208065d8b00ceb3bf68a677f641657318c24c
hyejin-kwon <hjkwon0507@findconnect.co.kr>
2022-01-03 14:17:10 +0900

    모각코 9일차 과제

--- Mogakko/youtube-clone/src/App.js
+++ Mogakko/youtube-clone/src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Subscription from './pages/Subscription';
+import Assignment from './pages/Assignment';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/explore" element={<Explore />} />
       <Route path="/subscription" element={<Subscription />} />
+      <Route path="/assignment" element={<Assignment />} />
     </Routes>
   );
 }
