2ae8429722f238621d41dd29b31dab1e03f55e84
hyejin-kwon <7applejink@gmail.com>
2022-01-24 18:02:59 +0900

    노마드코더

--- NomadCoder/nwitter/src/routes/Auth.js
+++ NomadCoder/nwitter/src/routes/Auth.js
@@ -3,24 +3,29 @@ import React, { useState } from "react";
 const Auth = () => {
   const [email, setEmail] =useState("");
   const [password, setPassword] =useState("");
+
   const onChange = (event) => {
-    const {target: {name, value}} = event;
+    const {
+      target: {name, value}
+    } = event;
     if(name === "email"){
       setEmail(value);
     } else if (name === "password"){
       setPassword(value);
     }
   };
+
   const onSubmit = (event) => {
     event.preventDefault();
-  }
+  };
+
   return (
   
     <div>
       <form>
         <input 
           name="email" 
-          type="text" 
+          type="email" 
           placeholder="Email" 
           required 
           value={email} 
