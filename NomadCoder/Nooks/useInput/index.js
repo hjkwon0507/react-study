74990d197982f2ab54db64230d425f98a66eaaf7
hyejin-kwon <7applejink@gmail.com>
2022-01-25 15:20:35 +0900

    노마드 영화웹

---
+++ NomadCoder/Nooks/useInput/index.js
@@ -0,0 +1,12 @@
+const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default App;
---
+++ NomadCoder/Nooks/useInput/useInput.js
@@ -0,0 +1,49 @@
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+
+// Class
+// class AppUgly extends React.Component {
+//   state = {
+//     item: 1
+//   };
+//   render() {
+//     const { item } = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello {item}</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={incrementItem}>Increment</button>
+//         <button onClick={decrementItem}>Decrement</button>
+//       </div>
+//     );
+//   }
+//   incrementItem = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item + 1
+//       };
+//     });
+//   };
+//   decrementItem = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item - 1
+//       };
+//     });
+//   };
+// }
