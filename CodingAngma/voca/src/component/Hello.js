4609a0736543f05689fd6023813602724f044de4
hyejin-kwon <7applejink@gmail.com>
2022-02-10 18:04:41 +0900

    코딩앙마 리액트

--- CodingAngma/voca/src/component/Hello.js
+++ CodingAngma/voca/src/component/Hello.js
@@ -1,26 +1,21 @@
-const Hello = () => {
-  function showName() {
-    console.log("Mike");
-  }
-  function showAge(age) {
-    console.log(age);
-  }
+import {useState} from "react";
+import UserName from "./UserName";
+
+const Hello = ({age}) => {
+  // let name = "Mike";
+  const [name, setName] = useState('Mike');
+  const msg = age > 19? "성인 입니다." : "미성년자 입니다.";
 
   return (
     <div>
-      <h1>Hello</h1>
-      <button onClick={showName}>Show name</button>
-      <button onClick={()=>{
-        showAge(30);
+      <h2>{name}({age}) : {msg}</h2>
+      <UserName name={name} />
+      <button onClick={() => {
+        setName(name === "Mike" ? "Jane" : "Mike");
         }}
-      >Show age
+      >
+        Change
       </button>
-      <input 
-        type="text" 
-        onChange={e => {
-          console.log(e.target.value);
-        }} 
-      />
     </div>
   )
 };
