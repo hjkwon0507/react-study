7a4b5b69e6292382397abefab695a4e035bfbef4
hyejin-kwon <7applejink@gmail.com>
2022-02-10 10:54:35 +0900

    코딩애플 Typescript 0210

--- CodingApple/TS/index.ts
+++ CodingApple/TS/index.ts
@@ -1,1 +1,23 @@
-// index.ts
+// Q2
+type MyType = {
+  color? : string,
+  size : number,
+  readonly position : number[]
+}
+
+let 테스트용변수 :MyType = {
+  size : 123,
+  position : [1,2,3]
+}
+
+// Q3
+type Info = {
+  name : string,
+  phone : number,
+  email? : string
+}
+
+let 검사 :Info = {
+  name : 'kim',
+  phone : 123
+}
