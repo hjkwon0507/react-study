a0a5c679c794444cdb35a5458306530945b92c29
hyejin-kwon <7applejink@gmail.com>
2022-02-14 11:49:21 +0900

    20220214

--- CodingAngma/voca/src/App.js
+++ CodingAngma/voca/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import Hello from './component/Hello'
-import styles from './App.module.css'
+// App.js
+
+import Day from './component/Day';
+import DayList from './component/DayList';
+import Header from './component/Header'
 
 function App() {
   return (
     <div className="App">
-      <Hello age={10} />
-      <Hello age={20} />
-      <Hello age={30} />
+      <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }
