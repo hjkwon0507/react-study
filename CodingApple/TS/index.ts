8d5c14b62241a3488de6cc95cd416b054460391f
hyejin-kwon <7applejink@gmail.com>
2022-02-15 12:06:04 +0900

    코딩애플 ts

--- CodingApple/TS/index.ts
+++ CodingApple/TS/index.ts
@@ -1,1 +1,20 @@
-// index.ts
+// Q1.
+interface Product {
+  brand : string,
+  serialNumber : number,
+  model : string[]
+}
+
+let 상품 :Product = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+
+// Q2.
+interface Cart {
+  product : string,
+  price : number
+}
+interface NewCart extends Cart {
+  card : boolean
+}
+
+
+let 장바구니 :Cart[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ] 
