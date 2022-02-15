e9edaa8ba16847004173fce4e32acff7c9c06c2d
hyejin-kwon <7applejink@gmail.com>
2022-02-15 11:20:36 +0900

    코딩앙마

--- CodingAngma/voca/src/App.js
+++ CodingAngma/voca/src/App.js
@@ -3,14 +3,19 @@
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<DayList />} /> 
+          <Route path="/day/:day" element={<Day />} /> 
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- CodingAngma/voca/src/component/Day.js
+++ CodingAngma/voca/src/component/Day.js
@@ -1,22 +1,25 @@
 // Day.js
-
 import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
 
 export default function Day() {
+  const { day } = useParams();
   // dummy.words
-  const day = 1;
-  const wordList = dummy.words.filter(word => word.day === day);
+  const wordList = dummy.words.filter(word => word.day === Number(day));
 
   return (
-    <table>
-      <tbody>
-        {wordList.map(word => (
-          <tr key={word.id}>
-            <td>{word.eng}</td>
-            <td>{word.kor}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map(word => (
+            <tr key={word.id}>
+              <td>{word.eng}</td>
+              <td>{word.kor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
