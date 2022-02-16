ab4aa2db7821110feb82748ab0ab57333d27135a
hyejin-kwon <7applejink@gmail.com>
2022-02-16 14:31:24 +0900

    코딩앙마 끝

--- CodingAngma/voca/src/App.js
+++ CodingAngma/voca/src/App.js
@@ -5,6 +5,8 @@ import DayList from './component/DayList';
 import Header from './component/Header'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import EmptyPage from './component/EmptyPage';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route exact path="/" element={<DayList />} /> 
           <Route path="/day/:day" element={<Day />} /> 
           <Route path="*" element={<EmptyPage />} /> 
+          <Route path="/create_word" element={<CreateWord />} /> 
+          <Route path="/create_day" element={<CreateDay />} /> 
         </Routes>
       </div>
     </BrowserRouter>
