b434f4092cd7a01487768279c25843c93ace6396
hyejin-kwon <7applejink@gmail.com>
2022-02-17 12:02:01 +0900

    코딩애플2

--- CodingApple/TS/index.ts
+++ CodingApple/TS/index.ts
@@ -1,15 +1,4 @@
 // index.ts
-type Car = {
-  wheel : '4개',
-  color : string
-}
-type Bike = {
-  wheel : '2개',
-  color : string
-}
-
-function 함수(x :Car | Bike){
-  if (x.wheel === '4개'){
-    console.log('x는 Car타입이에요')
-  }
+let 함수 = function(){
+  throw new Error()
 }
