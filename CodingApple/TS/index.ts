3bf13d0288d8e125231dd2bd728176b4e4dfcd24
hyejin-kwon <7applejink@gmail.com>
2022-02-17 14:41:56 +0900

    코딩애플3

--- CodingApple/TS/index.ts
+++ CodingApple/TS/index.ts
@@ -1,4 +1,15 @@
 // index.ts
-let 함수 = function(){
-  throw new Error()
+class User {
+  name :string;
+  private familyName :string = 'kim';
+  constructor(a){
+    this.name = a + this.familyName // 민수kim
+  }
+
+  이름변경함수(){
+    this.familyName = 'park'
+  }
 }
+let 유저1 = new User('민수');
+유저1.이름변경함수();
+console.log(유저1)
