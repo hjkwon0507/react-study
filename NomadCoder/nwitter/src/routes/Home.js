a6031f1010da460aab1786a00001db189d797d5f
hyejin-kwon <7applejink@gmail.com>
2022-03-13 20:19:19 +0900

    Nwitter Home.js input 추가

--- NomadCoder/nwitter/src/routes/Home.js
+++ NomadCoder/nwitter/src/routes/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => <div>
+  <form>
+    <input type="text" placeholder="What's on yout mind?" maxLength={120} />
+    <input type="submit" value="Nweet" />
+  </form>
+</div>
 
 export default Home;
