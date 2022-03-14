d446ff846e49a9a5369a589edb551da6ca1f63e5
hyejin-kwon <7applejink@gmail.com>
2022-03-14 16:06:53 +0900

    Home.js onChange, onSubmit 기능 추가

--- NomadCoder/nwitter/src/routes/Home.js
+++ NomadCoder/nwitter/src/routes/Home.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import { dbService } from "fbase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
-const Home = () => <div>
-  <form>
-    <input type="text" placeholder="What's on yout mind?" maxLength={120} />
-    <input type="submit" value="Nweet" />
-  </form>
-</div>
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async(event) => {
+    event.preventDefault();
+
+    await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createdAt: serverTimestamp(),
+      });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const { 
+      target:{value}
+    } = event;
+    setNweet(value);
+  };
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on yout mind?"
+          maxLength={120} 
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  )
+  
+}
+
 
 export default Home;
