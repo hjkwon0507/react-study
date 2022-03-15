cde1b3105df12d049d53abcf5340e47f945d0d73
hyejin-kwon <7applejink@gmail.com>
2022-03-15 17:24:44 +0900

    fastcampus 공부

--- fastcampus/styling2/src/components/StyledComponentsExample/StyledComponentsExample.jsx
+++ fastcampus/styling2/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function StyledComponentsExample() {
+  const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+  color: blue;
+
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+
+  & ~ & {
+    background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+  }
+
+  & + & {
+    background: lime; // <Thing> next to <Thing>
+  }
+
+  &.something {
+    background: orange; // <Thing> tagged with an additional CSS class ".something"
+  }
+
+  .something-else & {
+    border: 1px solid; // <Thing> inside another element labeled ".something-else"
+  }
+`
   // Create a Title component that'll render an <h1> tag with some styles
   const Title = styled.h1`
   font-size: 1.5em;
@@ -38,6 +61,14 @@ export default function StyledComponentsExample() {
   // Use Title and Wrapper like any other React component – except they're styled!
   return(
   <>
+    <Thing>Hello world!</Thing>
+    <Thing>How ya doing?</Thing>
+    <Thing className="something">The sun is shining...</Thing>
+    <div>Pretty nice day today.</div>
+    <Thing>Don't you think?</Thing>
+    <div className="something-else">
+      <Thing>Splendid.</Thing>
+    </div>
     <Wrapper>
       <Title>
         Hello World!
