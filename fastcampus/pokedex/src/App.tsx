12f982006e7b10b7f089bed5f71f0aff19b9ef4e
hyejin-kwon <7applejink@gmail.com>
2022-03-23 16:14:16 +0900

    pokedex 라우팅 및 react-query 세팅

--- fastcampus/pokedex/src/App.tsx
+++ fastcampus/pokedex/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path=":id" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
