de4682b05a30c0427a534e5dd635fdf1ec17fa02
hyejin-kwon <7applejink@gmail.com>
2022-04-03 20:19:19 +0900

    latestMovie, NowPlaying section 추가

--- fastcampus/watchout-pedia/src/features/movie/latest/index.tsx
+++ fastcampus/watchout-pedia/src/features/movie/latest/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "@emotion/styled";
 import useLatestMovie from "./useLatestMovie";
+import Card from "../../../components/Card";
 
 const Base = styled.div`
-  margin-bottom: 42px;
+  margin: 62px 0 42px;
 `;
 
 const Title = styled.h4`
@@ -18,14 +19,22 @@ const LatestMovieSection: React.FC = () => {
 
   const { data, isLoading } = useLatestMovie();
 
+  const getYear = (data: string) => data.split('-')[0];
+
   return (
     <Base>
       <Title>최근 개봉작</Title>
       {
-        isLoading ? (
+        isLoading || !data ? (
           <div>Loading...</div>
         ) : (
-          <div>{data?.data.title}</div>
+          <Card
+            linkUrl={`/movie/${data.data.id}`}
+            title={data.data.title}
+            posterPath={`${process.env.REACT_APP_IMAGE_PREFIX}/${data.data.poster_path}`}
+            voteAverage={data.data.vote_average}
+            year={getYear(data.data.release_date)}
+          />
         )
       }
     </Base>
