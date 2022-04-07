dbb8d42f3c3d2353ab8171ef1cbbc4e6185e06aa
hyejin-kwon <7applejink@gmail.com>
2022-04-07 11:27:39 +0900

    egoing onClick 이벤트 추가

--- Egoing/React2022/react-app/src/App.js
+++ Egoing/React2022/react-app/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function Header(props){
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
+
 function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
@@ -17,6 +21,7 @@ function Nav(props){
     </ol>
   </nav>
 }
+
 function Article(props){
   return <article>
     <h2>{props.title}</h2>
@@ -33,7 +38,9 @@ function App() {
   
   return (
     <div>
-      <Header title="REACT"></Header>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
       <Nav topics={topics}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
