73fe228ef1f945559f709fd8359048f926de3ee5
applejinn <7applejink@gmail.com>
2022-05-12 20:19:19 +0900

    블로그 제목 변경

--- CodingApple/react2022/blog/src/App.js
+++ CodingApple/react2022/blog/src/App.js
@@ -5,15 +5,15 @@ function App() {
 
   // post 라는 변수
   let post = '강남 우동 맛집';
-  let [a,b] = useState('남자 코트 추천');
+  let [글제목, b] = useState('여자 코트 추천');
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ a }</h4>
+        <h4>{ 글제목 }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
