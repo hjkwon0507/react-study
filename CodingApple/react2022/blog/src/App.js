53cfc69bd4dbd40ff5925632312654d63da37858
applejinn <7applejink@gmail.com>
2022-05-13 20:19:19 +0900

    글제목 세개 추가

--- CodingApple/react2022/blog/src/App.js
+++ CodingApple/react2022/blog/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // post 라는 변수
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState('여자 코트 추천');
+  let [글제목, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
 
   return (
     <div className="App">
@@ -13,7 +13,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{ 글제목 }</h4>
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
