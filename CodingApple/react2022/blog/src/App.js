4a1784829250ebe2108b7ac9ef645779a342335d
applejinn <7applejink@gmail.com>
2022-05-17 20:19:19 +0900

    글수정 버튼 추가

--- CodingApple/react2022/blog/src/App.js
+++ CodingApple/react2022/blog/src/App.js
@@ -13,6 +13,13 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={ ()=>{ setLike(like+1) } }>👍🏼</span> {like} </h4>
         <p>2월 17일 발행</p>
