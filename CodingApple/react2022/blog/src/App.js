3713321452a2558896fa7d0588635b67a7139420
applejinn <7applejink@gmail.com>
2022-05-20 20:19:19 +0900

    modal component 생성

--- CodingApple/react2022/blog/src/App.js
+++ CodingApple/react2022/blog/src/App.js
@@ -39,14 +39,20 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
 
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+      <Modal></Modal>
 
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
