922f1b589ddd7f15e2316b7168e5bf1e11cd1d6a
applejinn <7applejink@gmail.com>
2022-05-21 20:19:19 +0900

    fragment 추가

--- CodingApple/react2022/blog/src/App.js
+++ CodingApple/react2022/blog/src/App.js
@@ -47,11 +47,14 @@ function App() {
 
 function Modal(){
   return (
-    <div className="modal">
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
+    <>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      <div></div>
+    </>
   )
 }
 
