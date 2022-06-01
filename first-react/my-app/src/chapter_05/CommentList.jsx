5f1fa4db69ad60a23b44e38922550ae5a4da7273
applejinn <7applejink@gmail.com>
2022-06-01 20:19:19 +0900

    props 적용

--- first-react/my-app/src/chapter_05/CommentList.jsx
+++ first-react/my-app/src/chapter_05/CommentList.jsx
@@ -3,7 +3,7 @@ import Comment from "./Comment";
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"권혜진"} comment={"안녕하세요, 반갑습니다."} />
         </div>
     );
 }
