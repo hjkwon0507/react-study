66b96d3a73305ea59dcc861ddb034a47358047e6
applejinn <7applejink@gmail.com>
2022-06-03 20:19:19 +0900

    comments map 함수 추가

--- first-react/my-app/src/chapter_05/CommentList.jsx
+++ first-react/my-app/src/chapter_05/CommentList.jsx
@@ -1,9 +1,28 @@
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "권혜진",
+        comment: "안녕하세요, 첫번째 댓글입니다.",
+    },
+    {
+        name: "유재석",
+        comment: "안녕하세요, 두번째 댓글입니다.",
+    },
+    {
+        name: "조세호",
+        comment: "안녕하세요, 세번째 댓글입니다.",
+    },
+]
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"권혜진"} comment={"안녕하세요, 반갑습니다."} />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
         </div>
     );
 }
